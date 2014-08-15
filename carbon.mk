e26f6e978f2ea8ca9ecf23ff5727f9498fc84646
Alberto97 <albertop2197@gmail.com>
2014-08-15 14:14:56 +0200

    Carbonify
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

---
+++ carbon.mk
@@ -0,0 +1,21 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_RELEASE_NAME := MOTO G
+
+# Inherit from falcon device
+$(call inherit-product, device/motorola/falcon/device.mk)
+
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := carbon_falcon
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := falcon
+PRODUCT_MANUFACTURER := motorola

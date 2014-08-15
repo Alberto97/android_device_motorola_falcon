9dcd34fda8c7992e0d13622d051c64819ac0ebf2
Alberto97 <albertop2197@gmail.com>
2014-08-15 23:07:26 +0200

    Low Audio in Calls fixed
    Thanks to medic on XDA

--- carbon.mk
+++ carbon.mk
@@ -22,4 +22,5 @@ PRODUCT_MANUFACTURER := motorola
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
-    device/motorola/falcon/prebuilts/QuickBoot.apk:system/app/QuickBoot.apk
+    device/motorola/falcon/prebuilts/QuickBoot.apk:system/app/QuickBoot.apk \
+    device/motorola/falcon/configs/audio.primary.msm8226.so:system/lib/hw/audio.primary.msm8226.so
